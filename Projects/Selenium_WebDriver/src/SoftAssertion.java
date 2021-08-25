38db89fc356101a0813c49942b8267550fbe894b
Mshaveta <shaveta.malhotra47@gmail.com>
2021-08-25 12:10:07 +0530

    Added

---
+++ Projects/Selenium_WebDriver/src/SoftAssertion.java
@@ -0,0 +1,23 @@
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class SoftAssertion {
+	SoftAssert sa = new SoftAssert(); // it will perform E2E execution
+	
+	@Test
+	public void verifyTitle() {
+		System.out.println("Start Execution....");
+		String expcTitle = "facebook";
+		String actualTitle = "Facebook123";
+		
+		sa.assertEquals(actualTitle, expcTitle,"Expected and Actual title should be same!");
+		
+		System.out.println("End Execution....");
+		sa.assertAll();//for correct report generation
+	}
+	
+	 
+}
